2600cd81980ac3ed5fc95201b05b1205cb85b467
hugoo0605 <hugoserratgarcia@gmail.com>
2025-05-07 13:06:10 +0200

    prueba render1

--- src/main/java/org/bugbusters/Main.java
+++ src/main/java/org/bugbusters/Main.java
@@ -2,9 +2,11 @@ package org.bugbusters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EnableJpaRepositories("org.bugbusters.repository")
 public class Main {
     public static void main(String[] args) {
         System.out.println("Iniciando programa...");
